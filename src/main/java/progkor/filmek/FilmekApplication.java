57caba83cdb2174788827359b6e4b0e6b3815f02
PallaGergelyIstvan <pallagergelyistvan@gmail.com>
2022-05-24 04:51:07 +0200

    Hello World
    Hello World

---
+++ src/main/java/progkor/filmek/FilmekApplication.java
@@ -0,0 +1,8 @@
+package progkor.filmek;
+
+public class FilmekApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, szia, szevasz, van nálatok terasz? World.");
+    }
+}
